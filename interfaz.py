2d4f3577128980ab7596a3963ac5b95358cc3e9f
g451 <g451@gmail.com>
2024-06-11 22:21:50 -0300

    renombradas vars

--- interfaz.py
+++ interfaz.py
@@ -18,26 +18,26 @@ def Mensaje_Error(mensaje, titulo, resultado):
     nombreMensaje.pack()
 
 def llamar_TP():
-    pedido = cuadroPedido.get()
-    simulacion = cuadroSimulacion.get()
-    reposicion = cuadroReposicion.get()
-    costo = cuadroCosto.get()
-    precio = cuadroPrecio.get()
-    vto = cuadroVto.get()
-    recupero = cuadroRecupero.get()
-    stock = cuadroStock.get()
+    caja_cph = cuadrocaja_cph.get()
+    at_personalizada_cph = cuadroat_personalizada_cph.get()
+    tarj_credito_cph = cuadrotarj_credito_cph.get()
+    plazo_fijo_cph = cuadroplazo_fijo_cph.get()
+    prestamos_cph = cuadroprestamos_cph.get()
+    cantidad_cajeros_cph = cuadrocantidad_cajeros_cph.get()
+    cantidad_lineas_a_mostrar = cuadrocantidad_lineas_a_mostrar.get()
+    tiempo_simulacion = cuadrotiempo_simulacion.get()
     while True:
-        if int(pedido) >= 0 \
-                and int(simulacion) > 0 \
-                and int(reposicion) > 0 \
-                and int(costo) >= 0 \
-                and int(precio) >= 0 \
-                and int(vto) > 0 \
-                and int(recupero) >= 0 \
-                and int(stock) >= 0:
+        if int(caja_cph) >= 0 \
+                and int(at_personalizada_cph) > 0 \
+                and int(tarj_credito_cph) > 0 \
+                and int(plazo_fijo_cph) >= 0 \
+                and int(prestamos_cph) >= 0 \
+                and int(cantidad_cajeros_cph) > 0 \
+                and int(cantidad_lineas_a_mostrar) >= 0 \
+                and int(tiempo_simulacion) >= 0:
             Mensaje_Error("Ingreso satisfactorio", "¡¡Bien Hecho!!", True)
-            tp_tkinter(int(pedido), int(simulacion), int(reposicion),
-                       int(costo), int(precio), int(vto), int(stock), int(recupero)) # Aca llama al tp_tkinter
+            tp_tkinter(int(caja_cph), int(at_personalizada_cph), int(tarj_credito_cph),
+                       int(plazo_fijo_cph), int(prestamos_cph), int(cantidad_cajeros_cph), int(tiempo_simulacion), int(cantidad_lineas_a_mostrar)) # Aca llama al tp_tkinter
             break
         else:
             Mensaje_Error("INGRESO INCORRECTO", "¡¡ERROR!!", False)
@@ -54,50 +54,52 @@ raiz.resizable(width=False, height=False)
 back = "#FE9A2E"
 ventana.configure(background=back)
 
-cuadroReposicion=Entry(ventana, font=("Arial bold", 13))
-cuadroReposicion.grid(row=1,column=1)
-nombreReposicion=Label(ventana, text="Ingrese cada cuanto dias se hace la reposicion", font=("Arial bold", 13), background=back)
-nombreReposicion.grid(row=1, column=0)
+cuadrotarj_credito_cph=Entry(ventana, font=("Arial bold", 13))
+cuadrotarj_credito_cph.grid(row=1,column=1)
+nombretarj_credito_cph=Label(ventana, text="tarj_credito_cph", font=("Arial bold", 13), background=back)
+nombretarj_credito_cph.grid(row=1, column=0)
 
 
-cuadroPedido=Entry(ventana, font=("Arial bold", 13))
-cuadroPedido.grid(row=2,column=1)
-nombrePedido=Label(ventana, text="Ingrese el tamaño del pedido de reposicion", font=("Arial bold", 13), background=back)
-nombrePedido.grid(row=2, column=0)
+cuadrocaja_cph=Entry(ventana, font=("Arial bold", 13))
+cuadrocaja_cph.grid(row=2,column=1)
+nombrecaja_cph=Label(ventana, text="caja_cph", font=("Arial bold", 13), background=back)
+nombrecaja_cph.grid(row=2, column=0)
 
 
-cuadroSimulacion=Entry(ventana, font=("Arial bold", 13))
-cuadroSimulacion.grid(row=3,column=1)
-nombreSimulacion=Label(ventana, text="Ingrese la cantidad de dias a simular", font=("Arial bold", 13), background=back)
-nombreSimulacion.grid(row=3, column=0)
+cuadroat_personalizada_cph=Entry(ventana, font=("Arial bold", 13))
+cuadroat_personalizada_cph.grid(row=3,column=1)
+nombreat_personalizada_cph=Label(ventana, text="Atencion personalizada cph", font=("Arial bold", 13), background=back)
+nombreat_personalizada_cph.grid(row=3, column=0)
 
 
-cuadroVto=Entry(ventana, font=("Arial bold", 13))
-cuadroVto.grid(row=4,column=1)
-nombreVto=Label(ventana, text="Ingrese la cantidad de dias para descomposición", font=("Arial bold", 13), background=back)
-nombreVto.grid(row=4, column=0)
 
 
-cuadroCosto=Entry(ventana, font=("Arial bold", 13))
-cuadroCosto.grid(row=5,column=1)
-nombreCosto=Label(ventana, text="Ingrese el costo de compra del producto", font=("Arial bold", 13), background=back)
-nombreCosto.grid(row=5, column=0)
+
+cuadroplazo_fijo_cph=Entry(ventana, font=("Arial bold", 13))
+cuadroplazo_fijo_cph.grid(row=5,column=1)
+nombreplazo_fijo_cph=Label(ventana, text="plazo_fijo_cph", font=("Arial bold", 13), background=back)
+nombreplazo_fijo_cph.grid(row=5, column=0)
 
 
-cuadroPrecio=Entry(ventana, font=("Arial bold", 13))
-cuadroPrecio.grid(row=6,column=1)
-nombrePrecio=Label(ventana, text="Ingrese el precio de venta del producto", font=("Arial bold", 13), background=back)
-nombrePrecio.grid(row=6, column=0)
+cuadroprestamos_cph=Entry(ventana, font=("Arial bold", 13))
+cuadroprestamos_cph.grid(row=6,column=1)
+nombreprestamos_cph=Label(ventana, text="prestamos_cph", font=("Arial bold", 13), background=back)
+nombreprestamos_cph.grid(row=6, column=0)
 
-cuadroStock=Entry(ventana, font=("Arial bold", 13))
-cuadroStock.grid(row=7, column=1)
-nombreStock=Label(ventana, text="Ingrese el stock inicial", font=("Arial bold", 13), background=back)
-nombreStock.grid(row=7, column=0)
+cuadrocantidad_cajeros_cph=Entry(ventana, font=("Arial bold", 13))
+cuadrocantidad_cajeros_cph.grid(row=4,column=1)
+nombrecantidad_cajeros_cph=Label(ventana, text="cantidad de cajeros", font=("Arial bold", 13), background=back)
+nombrecantidad_cajeros_cph.grid(row=4, column=0)
 
-cuadroRecupero=Entry(ventana, font=("Arial bold", 13))
-cuadroRecupero.grid(row=8,column=1)
-nombreRecupero=Label(ventana, text="Ingrese el valor de recupero (si es cero se asume vencido)", font=("Arial bold", 13), background=back)
-nombreRecupero.grid(row=8, column=0)
+cuadrotiempo_simulacion=Entry(ventana, font=("Arial bold", 13))
+cuadrotiempo_simulacion.grid(row=7, column=1)
+nombretiempo_simulacion=Label(ventana, text="tiempo_simulacion", font=("Arial bold", 13), background=back)
+nombretiempo_simulacion.grid(row=7, column=0)
+
+cuadrocantidad_lineas_a_mostrar=Entry(ventana, font=("Arial bold", 13))
+cuadrocantidad_lineas_a_mostrar.grid(row=8,column=1)
+nombrecantidad_lineas_a_mostrar=Label(ventana, text="cantidad_lineas_a_mostrar", font=("Arial bold", 13), background=back)
+nombrecantidad_lineas_a_mostrar.grid(row=8, column=0)
 
 boton = Button(ventana, text="Aceptar", font=6, command=llamar_TP, width=8, background="#F5DA81")
 boton.grid(row=9, column=1)
